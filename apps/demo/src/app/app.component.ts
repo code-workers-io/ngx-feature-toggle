1704eeb6ab0e436f25ee459023b80a1bb0c5c2b5
Michael Berger <m.gonser@icloud.com>
2022-11-23 06:36:08 +0100

    Initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-feature-toggle-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
